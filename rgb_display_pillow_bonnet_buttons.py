6fa2c31fce5a27ac2fa2c3609e4d1c2f77200d88
rlaalsrl715 <79639282+rlaalsrl715@users.noreply.github.com>
2021-06-25 23:35:28 +0900

    Update rgb_display_pillow_bonnet_buttons.py

--- rgb_display_pillow_bonnet_buttons.py
+++ rgb_display_pillow_bonnet_buttons.py
@@ -106,9 +106,7 @@ ball_y1 = 210
 ball_y2 = 230
 while(True):
 	draw.ellipse((x1, y1, x2, y2), outline=button_outline, fill=udlr_fill)
-	disp.image(image)
 	draw.rectangle((0, 0, width, height), outline=0, fill=(0, 0, 0))
-	disp.image(image)
 	draw.ellipse((ball_x1, ball_y1, ball_x2, ball_y2), outline=button_outline, fill=button_fill)
 	if not button_L.value:
 		ball_x1 -= 20
