6f7b0bff65c764c22639e98af6f4530e79ab63d6
rlaalsrl715 <79639282+rlaalsrl715@users.noreply.github.com>
2021-06-27 23:27:55 +0900

    Update game.py

--- game.py
+++ game.py
@@ -142,21 +142,27 @@ def game_begin_inter(x1,x2,y1,y2,speed,name):
 		if(x1 >= ball_x1 and x2 <= ball_x2 and y2 >= ball_y1 and y2 <= ball_y2):
 			time.sleep(1)
 			return score
-		if(y1 <= ball_y1 and y2 <= ball_y2 and x2 >= ball_x1 and x2 <= ball_x2):
+		
+		if(y1 >= ball_y1 and y2 <= ball_y2 and x2 >= ball_x1 and x2 <= ball_x2):
 			time.sleep(1)
 			return score
-		if(y1 <= ball_y2 and y1 >= ball_y2 and x2 >= ball_x1 and x1 <= ball_x1):
+		
+		if(y1 <= ball_y2 and y1 >= ball_y1 and x2 >= ball_x1 and x2 <= ball_x2):
 			time.sleep(1)
 			return score
-		if(x1 >= ball_x1 and x2 <= ball_x2 and y1 >= ball_y2 and y2 >= ball_y2):
+		
+		if(x1 >= ball_x1 and x2 <= ball_x2 and y1 <= ball_y2 and y1 >= ball_y1):
 			time.sleep(1)
 			return score
-		if(x1 <= ball_x2 and x1 >= ball_x1 and  y1 >= ball_y2 and y2 >= ball_y2):
+		
+		if(x1 <= ball_x2 and x1 >= ball_x1 and  y1 <= ball_y2 and y1 >= ball_y1):
 			time.sleep(1)
 			return score
-		if(y1 <= ball_y1 and y2 <= ball_y2 and x1 <= ball_x2 and x1 >= ball_x1):
+		
+		if(y1 >= ball_y1 and y2 <= ball_y2 and x1 <= ball_x2 and x1 >= ball_x1):
 			time.sleep(1)
 			return score
+		
 		if not button_L.value:
 			ball_x1 -= 20
 			ball_x2 -= 20
