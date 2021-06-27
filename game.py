db7865c9296035de1cbe72fd035fe7bde3952062
rlaalsrl715 <79639282+rlaalsrl715@users.noreply.github.com>
2021-06-27 16:09:06 +0900

    Update game.py

--- game.py
+++ game.py
@@ -144,7 +144,8 @@ def play_game(x1,x2,y1,y2,speed,level,score):
 				x2 = x1+size
 		else:
 			draw.ellipse((x1, y1, x2, y2), outline=button_outline, fill=udlr_fill)
-			draw.ellipse((x3, y3, x4, y4), outline=button_outline, fill=udlr_fill)
+			if(x3< x2 and x4 < x2 and y3< y1 and y4 < y2)
+				draw.ellipse((x3, y3, x4, y4), outline=button_outline, fill=udlr_fill)
 			draw.ellipse((ball_x1, ball_y1, ball_x2, ball_y2), outline=button_outline, fill=button_fill)
 			disp.image(image)
 			draw.rectangle((0, 0, width, height), outline=0, fill=(0, 0, 0))
