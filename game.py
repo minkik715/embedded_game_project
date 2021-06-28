164bdaa21af3c9eb9972344eac94c78da78641df
rlaalsrl715 <79639282+rlaalsrl715@users.noreply.github.com>
2021-06-28 12:07:16 +0900

    Update game.py

--- game.py
+++ game.py
@@ -192,9 +192,117 @@ def game_begin_inter(x1,x2,y1,y2,speed,name):
 			x2 = x1+size
 		remove_screen()
 		time.sleep(0.01)
+
+def game_advance(x1,x2,y1,y2,speed,name)
+	ball_x1 = 110
+	ball_x2 = 130
+	ball_y1 = 110
+	ball_y2 = 130
+	draw.text((30, 50), name, font=fnt, fill=udlr_fill)
+	disp.image(image)
+	time.sleep(2)
+	score = 0
+	loc = 1
+	while(True):
+		rcolor = tuple(int(x * 255) for x in hsv_to_rgb(random.random(), 1, 1))
+		draw.ellipse((x1, y1, x2, y2), outline=button_outline, fill=rcolor)
+		draw.ellipse((ball_x1, ball_y1, ball_x2, ball_y2), outline=button_outline, fill=udlr_fill)
+		disp.image(image)
+		#1
+		if(x2 >= ball_x1 and x2 <= ball_x2 and y2 >= ball_y1 and y2 <= ball_y2):
+			time.sleep(1)
+			return score
+ 		#3
+		if(x1 <= ball_x2 and x1 >= ball_x1 and y2 >= ball_y1 and y2 <= ball_y2):
+			time.sleep(1)
+			return score
+		#2
+		if(x1 >= ball_x1 and x2 <= ball_x2 and y2 >= ball_y1 and y2 <= ball_y2):
+			time.sleep(1)
+			return score
+		#4
+		if(y1 >= ball_y1 and y2 <= ball_y2 and x2 >= ball_x1 and x2 <= ball_x2):
+			time.sleep(1)
+			return score
+		#5
+		if(y1 <= ball_y2 and y1 >= ball_y1 and x2 >= ball_x1 and x2 <= ball_x2):
+			time.sleep(1)
+			return score
+		#6
+		if(x1 >= ball_x1 and x2 <= ball_x2 and y1 <= ball_y2 and y1 >= ball_y1):
+			time.sleep(1)
+			return score
+		#7
+		if(x1 <= ball_x2 and x1 >= ball_x1 and  y1 <= ball_y2 and y1 >= ball_y1):
+			time.sleep(1)
+			return score
+		#8
+		if(y1 >= ball_y1 and y2 <= ball_y2 and x1 <= ball_x2 and x1 >= ball_x1):
+			time.sleep(1)
+			return score
+		#9
+		if(y1 >= ball_y1 and y2 <= ball_y2 and x1 >= ball_x1 and x2 <= ball_x2):
+			time.sleep(1)
+			return score
+		
+		if not button_L.value:
+			ball_x1 -= 20
+			ball_x2 -= 20
+		if not button_R.value:
+			ball_x1 += 20
+			ball_x2 += 20
+		if not button_U.value:
+			ball_y1 -= 20
+			ball_y2 -= 20
+		if not button_D.value:
+			ball_y1 += 20
+			ball_y2 += 20
+		if loc == 1 and y1 <= 250:
+			y1 = y1 +speed
+			y2 = y2 +speed
+		elif loc ==2 and x1 <= 250:
+			x1 = x1 + speed
+			x2 = x2 + speed
+		elif loc == 3 and y2 >= -10:
+			y1 = y1 - speed
+			y2 = y2 - speed
+		elif loc == 4 and x2 >= -10:
+			x1 = x1 - speed
+			x2 = x2 - speed
+		else:
+			score += 10
+			loc = random_location()
+			if loc == 1:
+				size = random.randint(20,40)
+				y1 = 0
+				y2 = y1+size
+				x1 = random.randint(0,200)
+				x2 = x1+size
+			if loc == 2:
+				size = random.randint(20,40)
+				y1 = random.randint(0,200)
+				y2 = y1+size
+				x1 = 0
+				x2 = x1+size			
+			if loc == 3:
+				size = random.randint(20,40)
+				y1 = 240 - size
+				y2 = 240
+				x1 = random.randint(0,200)
+				x2 = x1+size
+			if loc == 4:
+				size = random.randint(20,40)
+				y1 = random.randint(0,200)
+				y2 = y1+size
+				x1 = 240-size
+				x2 = 240
+		remove_screen()
+		time.sleep(0.01)
+
 	
-
-
+	
+	
+	
 def intro(B_color, I_color , A_color , E_color):
 	draw.text((20, 20), "Welcome", font=fnt, fill=udlr_fill)
 	draw.text((20, 50), "Avoid balls", font=fnt, fill=udlr_fill)
@@ -234,7 +342,7 @@ while True:
 			remove_screen()
 		if(tmp%4 == 2):
 			name = "Advance_Ver"
-			score = game_begin_inter(x1,x2,y1,y2,60,name)
+			score = game_advance(x1,x2,y1,y2,40,name)
 			game_end(score)
 			remove_screen()
 		if(tmp%4 == 3):
