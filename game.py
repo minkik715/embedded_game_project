4862ee2dd41c2da88dda6e5253b8aee932684efb
rlaalsrl715 <79639282+rlaalsrl715@users.noreply.github.com>
2021-06-28 12:45:55 +0900

    Update game.py

--- game.py
+++ game.py
@@ -116,7 +116,7 @@ def game_end(score):
 	remove_screen()
 
 	
-def crash_ball(x1, x2, y1, y2, ball_x1, ball_x2, ball_y1, ball_y2):
+def crash_ball(x1, x2, y1, y2, ball_x1, ball_x2, ball_y1, ball_y2,score):
 	
 	 #1
 	if(x2 >= ball_x1 and x2 <= ball_x2 and y2 >= ball_y1 and y2 <= ball_y2):
@@ -173,8 +173,9 @@ def game_begin_inter(x1,x2,y1,y2,speed,name):
 		draw.ellipse((x1, y1, x2, y2), outline=button_outline, fill=rcolor)
 		draw.ellipse((ball_x1, ball_y1, ball_x2, ball_y2), outline=button_outline, fill=udlr_fill)
 		disp.image(image)
-		check = crash_ball(x1, x2, y1, y1, ball_x1, ball_x2, ball_y1, ball_y2)
+		check = crash_ball(x1, x2, y1, y1, ball_x1, ball_x2, ball_y1, ball_y2,score)
 		if(check):
+			time.sleep(1)
 			return check
 		if not button_L.value:
 			ball_x1 -= 20
